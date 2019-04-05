27b8a73d7888f0a9ef30f834f2d0baaf17d1c8f5
kmietotwy <kmietotwy@gmail.com>
2019-04-05 11:29:23 -1000

    largest palindrome

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,18 +5,71 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function (digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
   // do your work here
+  function getfactor_0() {
+    return factor_0;
+  }
 
+  function getfactor_1() {
+    return factor_1;
+  }
 
+  function getpalindromeNumber() {
+    return palindromeNumber;
+  }
+
+  (function () {
+    //find max digit
+    let maxDigitStr = '';
+    let count = 0;
+    while (count < digits) {
+      maxDigitStr += '9';
+      count++;
+    }
+
+    let maxDigit = parseInt(maxDigitStr);
+
+    //find min digit
+    let minDigitStr = '1';
+    let minCount = 0;
+    while (minCount < digits - 1) {
+      minDigitStr += '0';
+      minCount++;
+    }
+
+    let minDigit = parseInt(minDigitStr);
+
+    //loop to find largest palindrome
+    for (let i = maxDigit; i > minDigit; i--) {
+      let currentPalindrome = 0;
+      for (let j = maxDigit; j > maxDigit - minDigit; j--) {
+        let product = i * j;
+        let multiple = parseInt(product.toString() + product);
+        let reversed = parseInt(multiple.toString().split('').reverse().join(''));
+        if (multiple === reversed && multiple > currentPalindrome) {
+          factor_0 = i;
+          factor_1 = j;
+          currentPalindrome = product;
+        }
+        if (currentPalindrome > palindromeNumber) {
+          palindromeNumber = currentPalindrome;
+          getpalindromeNumber();
+          getfactor_0();
+          getfactor_1();
+          return palindromeNumber;
+        }
+      }
+    }
+  }());
 
   return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
+    factor_0: factor_0,
+    factor_1: factor_1,
+    palindromeNumber: palindromeNumber
   };
 };
