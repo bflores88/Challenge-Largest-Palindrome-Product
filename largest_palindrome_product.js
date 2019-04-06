2ba006757ed6a8c1e10f77c1e544845b304684bc
kmietotwy <kmietotwy@gmail.com>
2019-04-06 12:30:48 -1000

    removed iife i think

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -11,44 +11,51 @@ module.exports = function (digits) {
   var palindromeNumber = 0;
 
   // do your work here
-  (function () {
-    //find max digit
-    let maxDigitStr = '';
-    let count = 0;
-    while (count < digits) {
-      maxDigitStr += '9';
-      count++;
-    }
+  //find max digit
+  let maxDigitStr = '';
+  let count = 0;
+  while (count < digits) {
+    maxDigitStr += '9';
+    count++;
+  }
 
-    let maxDigit = parseInt(maxDigitStr);
+  let maxDigit = parseInt(maxDigitStr);
 
-    //find min digit
-    let minDigitStr = '1';
-    let minCount = 0;
-    while (minCount < digits - 1) {
-      minDigitStr += '0';
-      minCount++;
-    }
+  //find min digit
+  let minDigitStr = '1';
+  let minCount = 0;
+  while (minCount < digits - 1) {
+    minDigitStr += '0';
+    minCount++;
+  }
 
-    let minDigit = parseInt(minDigitStr);
+  let minDigit = parseInt(minDigitStr);
 
-    //loop to find largest palindrome
-    for (let i = maxDigit; i > minDigit; i--) {
-      for (let j = maxDigit; j > maxDigit - minDigit; j--) {
-        let product = i * j;
-        let multiple = parseInt(product.toString() + product);
-        let reversed = parseInt(multiple.toString().split('').reverse().join(''));
-        if (multiple === reversed && product > palindromeNumber) {
-          factor_0 = i;
-          factor_1 = j;
-          palindromeNumber = product;
-          return factor_0, factor_1, palindromeNumber;
-        }
+  let palindromeObject = {
+    factor_0: factor_0,
+    factor_1: factor_1,
+    palindromeNumber: palindromeNumber
+  }
+
+  //loop to find largest palindrome
+  for (let i = maxDigit; i > maxDigit - minDigit; i--) {
+    for (let j = maxDigit; j > maxDigit - minDigit; j--) {
+      let product = i * j;
+      let multiple = parseInt(product.toString() + product);
+      let reversed = parseInt(multiple.toString().split('').reverse().join(''));
+      if (product > palindromeObject.palindromeNumber && multiple === reversed) {
+        palindromeObject.factor_0 = i;
+        palindromeObject.factor_1 = j;
+        palindromeObject.palindromeNumber = product;
       }
     }
-  }());
+  }
 
-  return { 
+  factor_0 = palindromeObject.factor_0;
+  factor_1 = palindromeObject.factor_1;
+  palindromeNumber = palindromeObject.palindromeNumber;
+
+  return {
     factor_0: factor_0,
     factor_1: factor_1,
     palindromeNumber: palindromeNumber
